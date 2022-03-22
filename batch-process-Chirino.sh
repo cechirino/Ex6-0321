9ec37f2b069aff9b85d4c76ded4dc420f360744c
cechirino <46880199+cechirino@users.noreply.github.com>
2022-03-21 20:37:21 -0500

    Update batch-process-Chirino.sh

--- batch-process-Chirino.sh
+++ batch-process-Chirino.sh
@@ -1,12 +1,10 @@
 #! /bin/sh 
 for f in "$1"/*; do
   if [ -f "$f" ]; then
+    FILE_NAME="$(basename "${f}")"
     SIZE="$(du -sh "${f}" | cut -f1)"
-    echo "Processing $f file..."
-    echo "$SIZE"
+    echo "File name: $FILE_NAME"
+    echo "File size: $SIZE"
   fi
 done
-
-
-
 
