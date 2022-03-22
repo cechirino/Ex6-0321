0c87401fbd60732832941516be0ceb5a6fc3fafe
cechirino <46880199+cechirino@users.noreply.github.com>
2022-03-21 21:15:50 -0500

    Update batch-process-Chirino.sh

--- batch-process-Chirino.sh
+++ batch-process-Chirino.sh
@@ -3,7 +3,7 @@ for f in "$1"/*; do
   if [ -f "$f" ]; then
     FILE_NAME="$(basename "${f}")"
     SIZE="$(du -sh "${f}" | cut -f1)"
-    WORD_COUNT="$(wc -w $FILE_NAME | cut -d' ' -f1)"  
+    WORD_COUNT="$(wc -w "${f}" | cut -d' ' -f1)"  
     echo "File name: $FILE_NAME"
     echo "File size: $SIZE"
     echo "Word count: $WORD_COUNT"
